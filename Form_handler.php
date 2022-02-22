2aa4499936d2bc4f8c21735edc4003eecf6cebb7
Mubashir <mubashir15700@gmail.com>
2022-02-22 21:38:50 +0530

    final commit

---
+++ Form_handler.php
@@ -0,0 +1,23 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_form = 'info@yourwebsite.com';
+$email_subject = 'new form submission';
+$email_body = "User name: $name.\n".
+              "User Email: $visitor_email.\n".
+              "User Subject: $subject.\n".
+              "User Message: $message.\n";
+
+$to = 'mubashir15700@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .="Reply-to: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+?>
